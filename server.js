2756f510b2ee9e2de74bf58ef6f096958942c9a2
sri-sunku <Sri.sunku@gmail.com>
2016-09-30 19:54:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
- res.send("Article one is requested and served here.");  
+ sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
  res.send("Article two is requested and served here.");  
