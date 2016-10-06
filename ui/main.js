3f97ea7f6ba8628716aaf545a7331d570aee8d1c
sri-sunku <Sri.sunku@gmail.com>
2016-10-06 22:43:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,18 @@ var element = document.getElementById('main-text'
 );
 
  element.innerHTML = 'New Value';
+
+//move the image
+
+var img = document.getElementById('madi');
+var marginLeft=0;
+
+function moveRight()
+{
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick= function()
+{
+    var interval = setInterval(moveRight,100);
+};
