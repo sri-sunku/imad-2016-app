85e36255f3351cb9391ac43de079c77d37d7337e
sri-sunku <Sri.sunku@gmail.com>
2016-10-06 22:27:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 //change the main text
-var element = document.getElementbyId('main-text'
+var element = document.getElementById('main-text'
 );
 
  element.innerHTML = 'New Value';
