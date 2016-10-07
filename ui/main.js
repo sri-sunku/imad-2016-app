62292d1738c20e29a622344f15d54f7ab98fad10
sri-sunku <Sri.sunku@gmail.com>
2016-10-07 13:21:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,9 @@ button.onclick = function (){
     
     // capture the response and store it in a variable
     request.onreadystatechange = function(){
-     if (req.readyState === XMLHttprequest.DONE){
+     if (request.readyState === XMLHttprequest.DONE){
          // take some action
-         if (req.status ===  200){
+         if (request.status ===  200){
              var counter = request.responseText;
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
